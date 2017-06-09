66174f3f19b904f33f56541d4990d6cbbe4cc935
tlambrou <lambrou@gmail.com>
2017-06-08 18:57:02 -0700

    App Setup

---
+++ Makestagram/Controllers/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Tassos Lambrou on 6/8/17.
+//  Copyright © 2017 SsosSoft. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Additional setup
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    
+  }
+  
+}
