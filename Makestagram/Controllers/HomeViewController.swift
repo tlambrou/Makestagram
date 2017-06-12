bd4a4f398678e8bca04863128940b20a8431ba3b
tlambrou <lambrou@gmail.com>
2017-06-11 22:27:41 -0700

    Completed section 16 from tutorial

--- Makestagram/Controllers/HomeViewController.swift
+++ Makestagram/Controllers/HomeViewController.swift
@@ -9,27 +9,40 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  // MARK: - Properties & Outlets
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  var posts = [Post]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    UserService.posts(for: User.current) { (posts) in
+      self.posts = posts
+      self.tableView.reloadData()
     }
-    */
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+}
 
+// MARK: - UITableViewDataSource
+
+extension HomeViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return posts.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
+    cell.backgroundColor = .red
+    
+    return cell
+  }
 }
